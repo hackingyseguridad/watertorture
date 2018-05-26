e6684668ced5ff7a9f938abb6b50d15448654864
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-05-27 00:57:49 +0200

    Update dnssec.sh

--- dnssec.sh
+++ dnssec.sh
@@ -1,3 +1,9 @@
+#
+# Script para test stress consultas al DNS autoritativo resolucion inversa de IP
+# Uso.: sh invesass.sh IP_publica
+# (c) www.hackingyseguridad.com 2017
+#
+
 #!/bin/bash
 echo "Uso: sh dnssec.sh dns_autoritativo dominio.com"
-while : do; dig @$1 $2 dnssec; done;
+while : ; do dig @$1 $2 dnssec; done;

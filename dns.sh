4ab15a2821f7adf3f584ac120d7b616ea7943acc
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-08-10 23:14:26 +0200

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -10,6 +10,7 @@ fi
 NS="$(dig $1 ns | egrep '[[:space:]]NS[[:space:]]' | awk '{print $5}')"
 echo "Servidores DNS autoritativos del dominio" $1
 echo $NS
+echo -e "\e[0;32m *** \e[0m"
 for NSERVER in $NS
 do
 dig @$NSERVER $1 any

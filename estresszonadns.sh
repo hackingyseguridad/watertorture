935fb7129e0598ff42bfc3d730281b7f9e2e4515
hackingyseguridad <antonio_taboada@hotmail.es>
2021-05-07 13:06:06 +0200

    Create estresszonadns.sh

---
+++ estresszonadns.sh
@@ -0,0 +1,18 @@
+# hackingyseguridad.com
+# test de stress zonas dns
+# Uso: sh estresszonadns.sh nombrezona.
+
+#!/bin/bash
+echo
+echo "Script consulta masiva resolución al DNS autoritativo"
+echo "Uso.: sh estressdns.sh dominio.com"
+echo
+
+contador=0
+
+while : ;do
+
+        dig NS $contador.$1
+        contador=`expr $contador + 1`;  echo "Consulta numero: " $contador
+
+done

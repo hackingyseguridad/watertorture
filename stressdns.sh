c00ac178f28c64c9e48f6ac1122399d100ce4472
hackingyseguridad <antonio_taboada@hotmail.es>
2022-11-25 12:05:27 +0100

    Update stressdns.sh

--- stressdns.sh
+++ stressdns.sh
@@ -1,20 +1,17 @@
 #
 # Script para test stress consultas al DNS autoritativo resolucion
 # Uso.: sh stressdns.sh dominio.com
-# (c) www.hackingyseguridad.com 2018
+# (c) www.hackingyseguridad.com 2022
 #
-
 #!/bin/bash
 echo
 echo "Script consulta masiva resolución al DNS autoritativo"
 echo "Uso.: sh stressdns.sh dominio.com"
 echo
-
 contador=0
-
 while : ;do
 
-        dig $contador.$1
-        contador=`expr $contador + 1`; 	echo "Consulta numero: " $contador
+        dig +short $contador.$1; echo $contador.$1
+        contador=`expr $contador + 1`;
 
 done
